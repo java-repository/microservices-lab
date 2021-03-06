ba03a4e1731877f1ffe7d084241b47d071b41c0e
Oracle Public Cloud User <opc@projector-0-1614357494.sub11181636450.micronautlabsvc.oraclevcn.com>
2021-03-06 05:19:53 +0000

    example

---
+++ example/src/main/java/example/micronaut/OwnerConfiguration.java
@@ -0,0 +1,32 @@
+package example.micronaut;
+
+import io.micronaut.context.annotation.EachProperty;
+import java.util.Collections;
+import java.util.List;
+
+@EachProperty("owners")
+public class OwnerConfiguration {
+
+    private String name;
+    private int age;
+    private List<String> pets = Collections.emptyList();
+
+    public List<String> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<String> pets) {
+        this.pets = pets;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    Owner create() {
+        return new Owner(name, age);
+    }
+}
